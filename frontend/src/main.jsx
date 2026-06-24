91eafc614537c0d1f6e160913dd4b6c92d3f9831
Ahmed Nasir Latif <freesurf264@gmail.com>
2026-06-24 21:26:29 +0500

    Add vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from '@vercel/analytics/react';
+
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { CartProvider } from './context/CartContext.jsx';
@@ -18,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <CartProvider>
             <Toaster position="top-right" />
             <App />
+            <Analytics/>
           </CartProvider>
         </AuthProvider>
       </BrowserRouter>
